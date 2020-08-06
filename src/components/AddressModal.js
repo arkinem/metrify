7169e7b4274aaad5412b79a794947e7c37682b45
arkinem <blazejgolinski@live.com>
2020-08-06 23:23:23 +0100

    adds debounced input with mock request

--- src/components/AddressModal.js
+++ src/components/AddressModal.js
@@ -1,11 +1,30 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import Close from "./CloseButton";
 import Screen from "./Screen";
 import { Input } from "@ui-kitten/components";
+import useDebounce from "../hooks/useDebounce";
 
 export default function AddressModal({ navigation }) {
-  const [value, setValue] = React.useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  useEffect(() => {
+    async function fetchData() {
+      setIsSearching(true);
+      const results = await searchCharacters(debouncedSearchTerm);
+      setResults(results);
+      setIsSearching(false);
+    }
+
+    if (debouncedSearchTerm.length > 2) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+  }, [debouncedSearchTerm]);
 
   return (
     <Screen noBackground>
@@ -13,8 +32,8 @@ export default function AddressModal({ navigation }) {
         <Input
           size="large"
           placeholder="Type localization here"
-          value={value}
-          onChangeText={setValue}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
         />
         <CloseButton onPress={() => navigation.goBack()} />
       </Heading>
@@ -32,3 +51,14 @@ const Heading = styled.View`
 const CloseButton = styled(Close)`
   margin-left: auto;
 `;
+
+// API search function
+async function searchCharacters(search) {
+  console.log("request", new Date());
+  await sleep(300);
+  return { text: "hehe" };
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
