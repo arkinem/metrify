c2c6ccd5d00629932330a8a4f23aa529994a7f55
arkinem <blazejgolinski@live.com>
2020-08-07 01:12:42 +0100

    extends Screen props

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,7 +4,13 @@ import styled from "styled-components/native";
 import Header from "./Header";
 import Svg, { Path } from "react-native-svg";
 
-export default function Screen({ title, headerProps, children, noBackground }) {
+export default function Screen({
+  title,
+  headerProps,
+  children,
+  noBackground,
+  plainView,
+}) {
   return (
     <Container>
       {title && <Header title={title} {...headerProps} />}
@@ -16,23 +22,32 @@ export default function Screen({ title, headerProps, children, noBackground }) {
           viewBox="0 0 1440 320"
         >
           <Path
-            fill="#0099ff"
+            fill="#51346F"
             fill-opacity="1"
             d="M0,64L120,74.7C240,85,480,107,720,101.3C960,96,1200,64,1320,48L1440,32L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
           />
         </Svg>
       )}
-      <Content
-        noBackground={noBackground}
-        contentContainerStyle={{
-          alignItems: "center",
-          justifyContent: "center",
-          paddingHorizontal: 20,
-          paddingVertical: 15,
-        }}
-      >
-        {children}
-      </Content>
+      {plainView ? (
+        <ContentView
+          noBackground={noBackground}
+          style={{ paddingHorizontal: 20, paddingVertical: 15 }}
+        >
+          {children}
+        </ContentView>
+      ) : (
+        <ContentScrollView
+          noBackground={noBackground}
+          contentContainerStyle={{
+            alignItems: "center",
+            justifyContent: "center",
+            paddingHorizontal: 20,
+            paddingVertical: 15,
+          }}
+        >
+          {children}
+        </ContentScrollView>
+      )}
     </Container>
   );
 }
@@ -42,8 +57,14 @@ const Container = styled.SafeAreaView`
   flex: 1;
 `;
 
-const Content = styled.ScrollView`
+const ContentView = styled.View`
   flex: 1;
   background-color: ${({ noBackground }) =>
-    noBackground ? "white" : "#0099ff"};
+    noBackground ? "white" : "#51346F"};
+`;
+
+const ContentScrollView = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ noBackground }) =>
+    noBackground ? "white" : "#51346F"};
 `;
