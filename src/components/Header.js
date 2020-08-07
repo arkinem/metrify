863b9cba3538b5dfa1b7b648def6af6b72370842
arkinem <blazejgolinski@live.com>
2020-08-07 19:45:44 +0100

    adds custom fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,45 +3,50 @@ import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { useIsDrawerOpen } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
+import { Text } from "@ui-kitten/components";
+import theme from "../constants/theme";
 
 export default function Header({ title, rightButton }) {
-  const isDrawerOpen = useIsDrawerOpen();
-  const { openDrawer, closeDrawer } = useNavigation();
+	const isDrawerOpen = useIsDrawerOpen();
+	const { openDrawer, closeDrawer } = useNavigation();
 
-  return (
-    <Container>
-      <DrawerButton
-        onPress={() => (!isDrawerOpen ? openDrawer() : closeDrawer())}
-      >
-        <Feather name="menu" size={30} color="black" />
-      </DrawerButton>
-      <Title>{title}</Title>
-      {rightButton && (
-        <RightSection justify="flexEnd">{rightButton}</RightSection>
-      )}
-    </Container>
-  );
+	return (
+		<Container>
+			<DrawerButton onPress={() => (!isDrawerOpen ? openDrawer() : closeDrawer())}>
+				<Feather name="menu" size={30} color="black" />
+			</DrawerButton>
+			<Title category="h4">{title}</Title>
+			<RightSection>{rightButton}</RightSection>
+		</Container>
+	);
 }
 
 const Container = styled.View`
-  height: 70px;
-  flex-direction: row;
-  align-items: center;
-  padding: 0 20px;
+	height: 70px;
+	flex-direction: row;
+	align-items: center;
+	padding: 0 20px;
+	justify-content: space-between;
 `;
 
 const DrawerButton = styled.TouchableOpacity`
-  height: 30px;
-  width: 30px;
-  margin-right: 10px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 40px;
+	width: 40px;
+	margin-right: 10px;
 `;
 
-const Title = styled.Text`
-  text-align: center;
-  margin-left: 80px;
-  font-size: 22px;
+const Title = styled(Text)`
+	font-family: ${theme.fonts.heading};
 `;
 
 const RightSection = styled.View`
-  margin-left: auto;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 40px;
+	height: 40px;
+	margin-left: 10px;
 `;
---
+++ src/constants/fonts.js
@@ -0,0 +1,11 @@
+import * as lato from "@expo-google-fonts/lato";
+import * as baloo from "@expo-google-fonts/baloo-tamma-2";
+
+export const appFonts = {
+	Baloo_400: baloo.BalooTamma2_400Regular,
+	Baloo_500: baloo.BalooTamma2_500Medium,
+	Baloo_600: baloo.BalooTamma2_600SemiBold,
+	Baloo_700: baloo.BalooTamma2_700Bold,
+	Lato_300: lato.Lato_300Light,
+	Lato_400: lato.Lato_400Regular,
+};
