353578a15a76edc066849645b480e77c917068ed
arkinem <blazejgolinski@live.com>
2020-08-07 23:35:08 +0100

    adds empty list handling

--- src/i18n/dictionary.js
+++ src/i18n/dictionary.js
@@ -1,4 +1,5 @@
 export default {
-  "addressModal.inputTitle": "Find propery address",
-  "addressModal.inputPlaceholder": "Start typing",
+	"addressModal.inputTitle": "Find propery address",
+	"addressModal.inputPlaceholder": "Start typing",
+	"addressModal.emptyList": "No matching results",
 };
